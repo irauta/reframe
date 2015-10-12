89c16eeb578a8af0a45cbd0f2ae8a3cc1ba51a72
Ilkka Rauta <ilkka.rauta@gmail.com>
2015-10-12 21:43:29 +0300

    Change trait definitions

--- src/traits.rs
+++ src/traits.rs
@@ -1,6 +1,12 @@
 
+use ::regl::VertexAttributeType;
 use ::attribute::BaseAttribute;
 
-pub trait ProvideBaseAttributes {
-    fn base_attributes() -> Vec<BaseAttribute>;
+pub trait VertexComponent {
+    fn attribute_type() -> VertexAttributeType;
+    fn component_count() -> u8;
+}
+
+pub trait NamedBaseAttributes {
+    fn attributes() -> Vec<(String, BaseAttribute)>;
 }
