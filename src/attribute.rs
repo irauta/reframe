a223f025318bb8eaff4413fbbc7dd235addfbb25
Ilkka Rauta <ilkka.rauta@gmail.com>
2015-11-07 21:45:04 +0200

    Refactor the attribute module
    Generate the attribute types completely within macro.
    Also add questionably useful fn new for all the types.

--- src/attribute.rs
+++ src/attribute.rs
@@ -36,13 +36,6 @@ pub fn named_attributes_to_indexed_attributes<M: MapNameToAttributeIndex>(
                     .collect()
 }
 
-#[repr(C,packed)]
-#[derive(Debug,Clone,Copy,Default)]
-pub struct Vec2Type<T: Debug + Clone + Copy + Default> {
-    pub x: T,
-    pub y: T,
-}
-
 pub type Vec2 = Vec2Type<f32>;
 pub type Vec2i8 = Vec2Type<i8>;
 pub type Vec2u8 = Vec2Type<u8>;
@@ -52,14 +45,6 @@ pub type Vec2i32 = Vec2Type<i32>;
 pub type Vec2u32 = Vec2Type<u32>;
 pub type Vec2f32 = Vec2Type<f32>;
 pub type Vec2f64 = Vec2Type<f64>;
-
-#[repr(C,packed)]
-#[derive(Debug,Clone,Copy,Default)]
-pub struct Vec3Type<T: Debug + Clone + Copy + Default> {
-    pub x: T,
-    pub y: T,
-    pub z: T,
-}
 
 pub type Vec3 = Vec3Type<f32>;
 pub type Vec3i8 = Vec3Type<i8>;
@@ -71,15 +56,6 @@ pub type Vec3u32 = Vec3Type<u32>;
 pub type Vec3f32 = Vec3Type<f32>;
 pub type Vec3f64 = Vec3Type<f64>;
 
-#[repr(C,packed)]
-#[derive(Debug,Clone,Copy,Default)]
-pub struct Vec4Type<T: Debug + Clone + Copy + Default> {
-    pub x: T,
-    pub y: T,
-    pub z: T,
-    pub w: T,
-}
-
 pub type Vec4 = Vec4Type<f32>;
 pub type Vec4i8 = Vec4Type<i8>;
 pub type Vec4u8 = Vec4Type<u8>;
@@ -89,14 +65,6 @@ pub type Vec4i32 = Vec4Type<i32>;
 pub type Vec4u32 = Vec4Type<u32>;
 pub type Vec4f32 = Vec4Type<f32>;
 pub type Vec4f64 = Vec4Type<f64>;
-
-#[repr(C,packed)]
-#[derive(Debug,Clone,Copy,Default)]
-pub struct RgbType<T: Debug + Clone + Copy + Default> {
-    pub r: T,
-    pub g: T,
-    pub b: T,
-}
 
 pub type Rgb = RgbType<u8>;
 pub type Rgbi8 = RgbType<i8>;
@@ -108,15 +76,6 @@ pub type Rgbu32 = RgbType<u32>;
 pub type Rgbf32 = RgbType<f32>;
 pub type Rgbf64 = RgbType<f64>;
 
-#[repr(C,packed)]
-#[derive(Debug,Clone,Copy,Default)]
-pub struct RgbaType<T: Debug + Clone + Copy + Default> {
-    pub r: T,
-    pub g: T,
-    pub b: T,
-    pub a: T,
-}
-
 pub type Rgba = RgbaType<u8>;
 pub type Rgbai8 = RgbaType<i8>;
 pub type Rgbau8 = RgbaType<u8>;
@@ -127,43 +86,57 @@ pub type Rgbau32 = RgbaType<u32>;
 pub type Rgbaf32 = RgbaType<f32>;
 pub type Rgbaf64 = RgbaType<f64>;
 
-macro_rules! impl_vertexcomponent {
-    ($impl_basetype:ident : $size:expr ) => (
-        impl_vertexcomponent!($impl_basetype<i8> : Byte, $size);
-        impl_vertexcomponent!($impl_basetype<u8> : UnsignedByte, $size);
-        impl_vertexcomponent!($impl_basetype<i16> : Short, $size);
-        impl_vertexcomponent!($impl_basetype<u16> : UnsignedShort, $size);
-        impl_vertexcomponent!($impl_basetype<i32> : Int, $size);
-        impl_vertexcomponent!($impl_basetype<u32> : UnsignedInt, $size);
-        impl_vertexcomponent!($impl_basetype<f32> : Float, $size);
-        impl_vertexcomponent!($impl_basetype<f64> : Double, $size);
+macro_rules! vertexcomponent_type {
+    ($impl_basetype:ident { $($field_name:ident),+ } ) => (
+        #[repr(C,packed)]
+        #[derive(Debug,Clone,Copy,Default)]
+        pub struct $impl_basetype<T: Debug + Clone + Copy + Default> {
+            $(pub $field_name: T),+
+        }
+        impl<T: Debug + Clone + Copy + Default> $impl_basetype<T> {
+            pub fn new($($field_name: T),+) -> $impl_basetype<T> {
+                $impl_basetype {
+                    $($field_name: $field_name),+
+                }
+            }
+        }
+        vertexcomponent_type!($impl_basetype<i8> : Byte { $($field_name),+ });
+        vertexcomponent_type!($impl_basetype<u8> : UnsignedByte { $($field_name),+ });
+        vertexcomponent_type!($impl_basetype<i16> : Short { $($field_name),+ });
+        vertexcomponent_type!($impl_basetype<u16> : UnsignedShort { $($field_name),+ });
+        vertexcomponent_type!($impl_basetype<i32> : Int { $($field_name),+ });
+        vertexcomponent_type!($impl_basetype<u32> : UnsignedInt { $($field_name),+ });
+        vertexcomponent_type!($impl_basetype<f32> : Float { $($field_name),+ });
+        vertexcomponent_type!($impl_basetype<f64> : Double { $($field_name),+ });
     );
-    ($impl_type:ty : $attribute_type:ident, $size:expr ) => (
+    ($impl_type:ty : $attribute_type:ident { $($field_name:ident),+ } ) => (
         impl ::attribute::VertexComponent for $impl_type {
             fn attribute_type() -> ::regl::VertexAttributeType {
                 ::regl::VertexAttributeType::$attribute_type
             }
             fn component_count() -> u8 {
-                $size
+                // The repetition needs to refer to a variable within the repetition, so let's be
+                // redundant and refer to a variable within the repetition
+                0 $(+ { let $field_name = 1; $field_name })+
             }
         }
     );
 }
 
-impl_vertexcomponent!(Vec2Type : 2);
-impl_vertexcomponent!(Vec3Type : 3);
-impl_vertexcomponent!(Vec4Type : 4);
-impl_vertexcomponent!(RgbType : 3);
-impl_vertexcomponent!(RgbaType : 4);
+vertexcomponent_type!(Vec2Type { x, y });
+vertexcomponent_type!(Vec3Type { x, y, z });
+vertexcomponent_type!(Vec4Type { x, y, z, w });
+vertexcomponent_type!(RgbType { r, g, b });
+vertexcomponent_type!(RgbaType { r, g, b, a });
 
-impl_vertexcomponent!(i8 : Byte, 1);
-impl_vertexcomponent!(u8 : UnsignedByte, 1);
-impl_vertexcomponent!(i16 : Short, 1);
-impl_vertexcomponent!(u16 : UnsignedShort, 1);
-impl_vertexcomponent!(i32 : Int, 1);
-impl_vertexcomponent!(u32 : UnsignedInt, 1);
-impl_vertexcomponent!(f32 : Float, 1);
-impl_vertexcomponent!(f64 : Double, 1);
+vertexcomponent_type!(i8 : Byte { x });
+vertexcomponent_type!(u8 : UnsignedByte { x });
+vertexcomponent_type!(i16 : Short { x });
+vertexcomponent_type!(u16 : UnsignedShort { x });
+vertexcomponent_type!(i32 : Int { x });
+vertexcomponent_type!(u32 : UnsignedInt { x });
+vertexcomponent_type!(f32 : Float { x });
+vertexcomponent_type!(f64 : Double { x });
 
 #[derive(Debug,Clone,Copy)]
 pub struct BaseAttribute {
